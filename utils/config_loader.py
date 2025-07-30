312a8a1a012103c03832216906a0436e430842ec
vikasjangid10 <v7776139@gmail.com>
2025-07-30 12:17:03 +0530

    config changes

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,7 +1,13 @@
 import yaml
 
 
-def load_config(config_path: str = "config\config.yaml") -> dict:
+import os
+
+def load_config(config_path: str = None) -> dict:
+    # Use forward slashes and absolute path for compatibility
+    if config_path is None:
+        config_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'config.yaml')
+        config_path = os.path.abspath(config_path)
     with open(config_path, "r") as file:
-        config=yaml.safe_load(file)
+        config = yaml.safe_load(file)
     return config
